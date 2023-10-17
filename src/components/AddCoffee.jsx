a370b0e69165c16881eac55cd35a5fb78b921278
Kasem32833 <abulkasem32833@gmail.com>
2023-10-17 13:56:53 +0300

    Coffee shop client

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <p>Add coffee</p>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,10 @@
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <p>Update coffee</p>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
